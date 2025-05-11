31ae81d6f191d0102d6d6187c139e39848a3a65e
Khoimap-max <donguyenkhoi1901@gmail.com>
2025-05-11 12:00:13 +0700

    upload

--- Khoa_3/Lesson_1/app.js
+++ Khoa_3/Lesson_1/app.js
@@ -5,3 +5,17 @@ fetch('https://pokeapi.co/api/v2/pokemon/10')
         let img = document.getElementById('img')
         img.src = data.sprites.front_default;
     })
+
+let btn= document.getElementById('btn')
+btn.addEventListener('click', getImg)
+
+function getImg(){
+    let id=document.querySelector('input').value;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        let img = document.getElementById('img')
+        img.src = data.sprites.front_default;
+    })
+}
